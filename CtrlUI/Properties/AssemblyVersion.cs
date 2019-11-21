48cf9c4d3eef9b4f4b7cb027e0de1e3c9e231715
Arnold Vink <info@arnoldvink.com>
2019-11-21 18:47:16 +0100

    - Updated the default Epic Games logo.

--- CtrlUI/Properties/AssemblyVersion.cs
+++ CtrlUI/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly 
-[assembly: AssemblyVersion("1.9.8.1")]
-[assembly: AssemblyFileVersion("1.9.8.1")]
+[assembly: AssemblyVersion("1.9.8.2")]
+[assembly: AssemblyFileVersion("1.9.8.2")]
