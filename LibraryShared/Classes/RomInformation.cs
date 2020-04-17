04d38fb59166fc1085e0bd471477d7417932780d
Arnold Vink <info@arnoldvink.com>
2020-04-17 14:09:08 +0200

    - Added rom information download code.

---
+++ LibraryShared/Classes/RomInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace LibraryShared
+{
+    public partial class Classes
+    {
+        public class RomInformation
+        {
+            public BitmapImage RomImageBitmap { get; set; }
+            public string RomDescription { get; set; }
+        }
+    }
+}
