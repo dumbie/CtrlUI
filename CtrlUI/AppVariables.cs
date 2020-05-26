5fa046f01f0c88df33ae856b1e307dc2ad3f538d
Arnold Vink <info@arnoldvink.com>
2020-05-26 13:56:03 +0200

    - Improved keyboard clicking sounds by cutting off silence.

--- CtrlUI/AppVariables.cs
+++ CtrlUI/AppVariables.cs
@@ -134,7 +134,7 @@ namespace CtrlUI
         public static bool vControllerBusy = false;
         public static int vControllerDelayNanoTicks = 10;
         public static int vControllerDelayMicroTicks = 75;
-        public static int vControllerDelayShortTicks = 130;
+        public static int vControllerDelayShortTicks = 125;
         public static int vControllerDelayMediumTicks = 250;
         public static int vControllerDelayLongTicks = 750;
         public static int vControllerDelay_DPad = Environment.TickCount;
