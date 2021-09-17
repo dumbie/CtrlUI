9a1d01e955fdda8146c93d6b940c276cb4a9e52c
Arnold Vink <info@arnoldvink.com>
2021-09-17 13:56:02 +0200

    DirectXInput: Added code to enable keyboard num lock.

---
+++ DirectXInput/Resources/InputOutput/OutputKeyboard.cs
@@ -0,0 +1,42 @@
+﻿using ArnoldVinkCode;
+using System.Windows.Input;
+using static DirectXInput.AppVariables;
+using static LibraryUsb.FakerInputDevice;
+
+namespace DirectXInput
+{
+    partial class WindowMain
+    {
+        //Disable hardware capslock
+        public static void DisableHardwareCapsLock()
+        {
+            try
+            {
+                AVActions.ActionDispatcherInvoke(delegate
+                {
+                    if (System.Windows.Input.Keyboard.GetKeyStates(Key.CapsLock) == KeyStates.Toggled)
+                    {
+                        vFakerInputDevice.KeyboardPressRelease(KeyboardModifiers.None, KeyboardKeys.CapsLock, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None);
+                    }
+                });
+            }
+            catch { }
+        }
+
+        //Enable hardware numlock
+        public static void EnableHardwareNumLock()
+        {
+            try
+            {
+                AVActions.ActionDispatcherInvoke(delegate
+                {
+                    if (System.Windows.Input.Keyboard.GetKeyStates(Key.NumLock) != KeyStates.Toggled)
+                    {
+                        vFakerInputDevice.KeyboardPressRelease(KeyboardModifiers.None, KeyboardKeys.NumLock, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None, KeyboardKeys.None);
+                    }
+                });
+            }
+            catch { }
+        }
+    }
+}
