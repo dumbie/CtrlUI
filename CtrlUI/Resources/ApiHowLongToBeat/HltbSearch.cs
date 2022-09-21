6cfd0f4fc2016b2da54340197a5d8d4d856789de
Arnold Vink <info@arnoldvink.com>
2022-09-21 11:23:09 +0200

    - CtrlUI: Started adding HowLongToBeat api code.

---
+++ CtrlUI/Resources/ApiHowLongToBeat/HltbSearch.cs
@@ -0,0 +1,84 @@
+﻿using ArnoldVinkCode;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static LibraryShared.Classes;
+
+namespace CtrlUI
+{
+    partial class WindowMain
+    {
+        //Search and download game information
+        public async Task<ApiHltbSearchResult> ApiHowLongToBeat_Search(string gameName)
+        {
+            try
+            {
+                Debug.WriteLine("Searching how long to beat game: " + gameName);
+
+                //Set request headers
+                string[] requestAccept = new[] { "Accept", "application/json" };
+                string[] requestReferer = new[] { "Referer", "https://howlongtobeat.com" };
+                //Xbox string[] requestAuthorization = new[] { "Authorization", "Basic bWljcm9zb2Z0X3N0b3JlZnJvbnQ6MmZhMDcxNDU2ZDJhZDNiNmI2M2Y2N2Q4NGVhYzcwNTY=" };
+                string[][] requestHeaders = new string[][] { requestAccept, requestReferer };
+
+                //Set download uri
+                //Xbox string apiUrl = "https://howlongtobeat.com/___api/games?xbox_id=9WZDNCRFHWD2";
+                string apiUrl = "https://www.howlongtobeat.com/api/search";
+
+                //Create json request
+                ApiHltbSearchQuery jsonSearchQuery = new ApiHltbSearchQuery();
+                jsonSearchQuery.searchTerms = new[] { gameName };
+                string jsonRequest = JsonConvert.SerializeObject(jsonSearchQuery);
+
+                //Create request body
+                StringContent requestBodyStringContent = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                //Create request uri
+                Uri requestUri = new Uri(apiUrl);
+
+                //Authenticate with Twitch
+                string resultSearch = await AVDownloader.SendPostRequestAsync(5000, "CtrlUI", requestHeaders, requestUri, requestBodyStringContent);
+                if (string.IsNullOrWhiteSpace(resultSearch))
+                {
+                    Debug.WriteLine("Failed downloading how long to beat, no connection.");
+                    return null;
+                }
+
+                //Deserialize json string
+                ApiHltbSearchResult jsonSearchResult = JsonConvert.DeserializeObject<ApiHltbSearchResult>(resultSearch);
+                foreach (ApiHltbSearchResult.Data hltbData in jsonSearchResult.data)
+                {
+                    Debug.WriteLine(hltbData.game_name);
+                    if (hltbData.comp_all_count <= 0)
+                    {
+                        Debug.WriteLine("Unknown gameplay time");
+                    }
+                    else
+                    {
+                        if (hltbData.comp_main > 0)
+                        {
+                            Debug.WriteLine("Maingame" + AVFunctions.SecondsToHms(hltbData.comp_main, true, false));
+                        }
+                        else
+                        {
+                            Debug.WriteLine("MaingameUnknown");
+                        }
+                        Debug.WriteLine("Main + Side" + AVFunctions.SecondsToHms(hltbData.comp_plus, true, false));
+                        Debug.WriteLine("100% done" + AVFunctions.SecondsToHms(hltbData.comp_100, true, false));
+                        Debug.WriteLine("Completed " + hltbData.comp_all_count + " times");
+                    }
+                }
+
+                return jsonSearchResult;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed searching how long to beat: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ LibraryShared/Classes/ApiHltb.cs
@@ -0,0 +1,104 @@
+﻿namespace LibraryShared
+{
+    public partial class Classes
+    {
+        public class ApiHltbSearchQuery
+        {
+            public string searchType { get; set; } = "games";
+            public string[] searchTerms { get; set; }
+            public int searchPage { get; set; } = 1;
+            public int size { get; set; } = 20;
+            public SearchOptions searchOptions { get; set; } = new SearchOptions();
+
+            public class SearchOptions
+            {
+                public Games games { get; set; } = new Games();
+                public Users users { get; set; } = new Users();
+                public string filter { get; set; } = string.Empty;
+                public int sort { get; set; }
+                public int randomizer { get; set; }
+            }
+
+            public class Games
+            {
+                public int userId { get; set; }
+                public string platform { get; set; } = string.Empty;
+                public string sortCategory { get; set; } = "popular";
+                public string rangeCategory { get; set; } = "main";
+                public RangeTime rangeTime { get; set; } = new RangeTime();
+                public Gameplay gameplay { get; set; } = new Gameplay();
+                public string modifier { get; set; } = string.Empty;
+            }
+
+            public class RangeTime
+            {
+                public int min { get; set; }
+                public int max { get; set; }
+            }
+
+            public class Gameplay
+            {
+                public string perspective { get; set; } = string.Empty;
+                public string flow { get; set; } = string.Empty;
+                public string genre { get; set; } = string.Empty;
+            }
+
+            public class Users
+            {
+                public string sortCategory { get; set; } = "postcount";
+            }
+        }
+
+        public class ApiHltbSearchResult
+        {
+            public string color { get; set; }
+            public string title { get; set; }
+            public string category { get; set; }
+            public int count { get; set; }
+            public int pageCurrent { get; set; }
+            public int pageTotal { get; set; }
+            public int pageSize { get; set; }
+            public Data[] data { get; set; }
+
+            public class Data
+            {
+                public int count { get; set; }
+                public int game_id { get; set; }
+                public string game_name { get; set; }
+                public int game_name_date { get; set; }
+                public string game_alias { get; set; }
+                public string game_type { get; set; }
+                public string game_image { get; set; }
+                public int comp_lvl_combine { get; set; }
+                public int comp_lvl_sp { get; set; }
+                public int comp_lvl_co { get; set; }
+                public int comp_lvl_mp { get; set; }
+                public int comp_lvl_spd { get; set; }
+                public int comp_main { get; set; }
+                public int comp_plus { get; set; }
+                public int comp_100 { get; set; }
+                public int comp_all { get; set; }
+                public int comp_main_count { get; set; }
+                public int comp_plus_count { get; set; }
+                public int comp_100_count { get; set; }
+                public int comp_all_count { get; set; }
+                public int invested_co { get; set; }
+                public int invested_mp { get; set; }
+                public int invested_co_count { get; set; }
+                public int invested_mp_count { get; set; }
+                public int count_comp { get; set; }
+                public int count_speedrun { get; set; }
+                public int count_backlog { get; set; }
+                public int count_review { get; set; }
+                public int review_score { get; set; }
+                public int count_playing { get; set; }
+                public int count_retired { get; set; }
+                public string profile_dev { get; set; }
+                public int profile_popular { get; set; }
+                public int profile_steam { get; set; }
+                public string profile_platform { get; set; }
+                public int release_world { get; set; }
+            }
+        }
+    }
+}
