90d10983d6c18deb0d4721e957ed30b2371b8f04
Arnold Vink <info@arnoldvink.com>
2023-03-18 17:47:24 +0100

    - FpsOverlayer: Added Open Cross, Square and Circle crosshair styles.

--- FpsOverlayer/OverlayCrosshair.cs
+++ FpsOverlayer/OverlayCrosshair.cs
@@ -44,32 +44,51 @@ namespace FpsOverlayer
             try
             {
                 //Change the crosshair style
+                crosshair_Dot.Visibility = Visibility.Collapsed;
+                crosshair_Circle.Visibility = Visibility.Collapsed;
+                crosshair_Squarebox.Visibility = Visibility.Collapsed;
+                crosshair_CrossClosed.Visibility = Visibility.Collapsed;
+                crosshair_CrossOpen.Visibility = Visibility.Collapsed;
+                crosshair_LineHorizontal.Visibility = Visibility.Collapsed;
                 int crosshairStyle = SettingLoad(vConfigurationFpsOverlayer, "CrosshairStyle", typeof(int));
                 if (crosshairStyle == 0)
                 {
                     crosshair_Dot.Visibility = Visibility.Visible;
-                    crosshair_Line.Visibility = Visibility.Collapsed;
-                    crosshair_Cross.Visibility = Visibility.Collapsed;
                 }
                 else if (crosshairStyle == 1)
                 {
-                    crosshair_Dot.Visibility = Visibility.Collapsed;
-                    crosshair_Line.Visibility = Visibility.Collapsed;
-                    crosshair_Cross.Visibility = Visibility.Visible;
+                    crosshair_Circle.Visibility = Visibility.Visible;
+                }
+                else if (crosshairStyle == 2)
+                {
+                    crosshair_Squarebox.Visibility = Visibility.Visible;
+                }
+                else if (crosshairStyle == 3)
+                {
+                    crosshair_CrossClosed.Visibility = Visibility.Visible;
+                }
+                else if (crosshairStyle == 4)
+                {
+                    crosshair_CrossOpen.Visibility = Visibility.Visible;
                 }
                 else
                 {
-                    crosshair_Dot.Visibility = Visibility.Collapsed;
-                    crosshair_Line.Visibility = Visibility.Visible;
-                    crosshair_Cross.Visibility = Visibility.Collapsed;
+                    crosshair_LineHorizontal.Visibility = Visibility.Visible;
                 }
 
                 //Change the crosshair color
                 string crosshairColor = SettingLoad(vConfigurationFpsOverlayer, "CrosshairColor", typeof(string));
-                crosshair_Dot.Background = new BrushConverter().ConvertFrom(crosshairColor) as SolidColorBrush;
-                crosshair_Line.Fill = new BrushConverter().ConvertFrom(crosshairColor) as SolidColorBrush;
-                crosshair_Cross_Vertical.Fill = new BrushConverter().ConvertFrom(crosshairColor) as SolidColorBrush;
-                crosshair_Cross_Horizontal.Fill = new BrushConverter().ConvertFrom(crosshairColor) as SolidColorBrush;
+                SolidColorBrush crosshairBrush = new BrushConverter().ConvertFrom(crosshairColor) as SolidColorBrush;
+                crosshair_Dot.Background = crosshairBrush;
+                crosshair_Circle.Stroke = crosshairBrush;
+                crosshair_Squarebox.BorderBrush= crosshairBrush;
+                crosshair_LineHorizontal.Fill = crosshairBrush;
+                crosshair_CrossClosed_Vertical.Fill = crosshairBrush;
+                crosshair_CrossClosed_Horizontal.Fill = crosshairBrush;
+                crosshair_CrossOpen_VerticalTop.Fill = crosshairBrush;
+                crosshair_CrossOpen_VerticalBottom.Fill = crosshairBrush;
+                crosshair_CrossOpen_HorizontalLeft.Fill = crosshairBrush;
+                crosshair_CrossOpen_HorizontalRight.Fill = crosshairBrush;
 
                 int crosshairSize = SettingLoad(vConfigurationFpsOverlayer, "CrosshairSize", typeof(int));
                 int crosshairThickness = SettingLoad(vConfigurationFpsOverlayer, "CrosshairThickness", typeof(int));
@@ -79,15 +98,37 @@ namespace FpsOverlayer
                 crosshair_Dot.Height = crosshairSize;
                 crosshair_Dot.CornerRadius = new CornerRadius(crosshairSize);
 
-                //Change the crosshair size - cross
-                crosshair_Cross_Vertical.Width = crosshairThickness;
-                crosshair_Cross_Vertical.Height = crosshairSize;
-                crosshair_Cross_Horizontal.Width = crosshairSize;
-                crosshair_Cross_Horizontal.Height = crosshairThickness;
+                //Change the crosshair size - circle
+                crosshair_Circle.Width = crosshairSize;
+                crosshair_Circle.Height = crosshairSize;
+                crosshair_Circle.StrokeThickness = crosshairThickness;
+
+                //Change the crosshair size - squarebox
+                crosshair_Squarebox.Width = crosshairSize;
+                crosshair_Squarebox.Height = crosshairSize;
+                crosshair_Squarebox.BorderThickness = new Thickness(crosshairThickness);
+
+                //Change the crosshair size - cross closed
+                crosshair_CrossClosed_Vertical.Width = crosshairThickness;
+                crosshair_CrossClosed_Vertical.Height = crosshairSize;
+                crosshair_CrossClosed_Horizontal.Width = crosshairSize;
+                crosshair_CrossClosed_Horizontal.Height = crosshairThickness;
+
+                //Change the crosshair size - cross open
+                crosshair_CrossOpen_Center.Width = crosshairSize;
+                crosshair_CrossOpen_Center.Height = crosshairSize;
+                crosshair_CrossOpen_VerticalTop.Width = crosshairThickness;
+                crosshair_CrossOpen_VerticalTop.Height = crosshairSize;
+                crosshair_CrossOpen_VerticalBottom.Width = crosshairThickness;
+                crosshair_CrossOpen_VerticalBottom.Height = crosshairSize;
+                crosshair_CrossOpen_HorizontalLeft.Width = crosshairSize;
+                crosshair_CrossOpen_HorizontalLeft.Height = crosshairThickness;
+                crosshair_CrossOpen_HorizontalRight.Width = crosshairSize;
+                crosshair_CrossOpen_HorizontalRight.Height = crosshairThickness;
 
                 //Change the crosshair size - line
-                crosshair_Line.Width = crosshairSize;
-                crosshair_Line.Height = crosshairThickness;
+                crosshair_LineHorizontal.Width = crosshairSize;
+                crosshair_LineHorizontal.Height = crosshairThickness;
 
                 //Change the crosshair opacity
                 grid_CrosshairOverlayer.Opacity = SettingLoad(vConfigurationFpsOverlayer, "CrosshairOpacity", typeof(double));
