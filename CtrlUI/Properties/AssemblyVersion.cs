afd3e1b48edaf751cf450aa713996000704b1198
Arnold Vink <info@arnoldvink.com>
2023-05-12 11:24:13 +0200

    - CtrlUI: Added capture sounds.

--- CtrlUI/Properties/AssemblyVersion.cs
+++ CtrlUI/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly
-[assembly: AssemblyVersion("1.95.2.0")]
-[assembly: AssemblyFileVersion("1.95.2.0")]
+[assembly: AssemblyVersion("1.96.0.0")]
+[assembly: AssemblyFileVersion("1.96.0.0")]
