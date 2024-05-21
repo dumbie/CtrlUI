481980e33ad29fd56637b20891d9ee08cc470092
Arnold Vink <vink_arnold@hotmail.com>
2024-05-21 15:57:45 +0200

    - DirectXInput: Added Nintendo Switch Pro controller mode validation. - DirectXInput: Added 8BitDo Pro 2 hash validation in PS4 mode.

--- DirectXInput/Input/InputValidate.cs
+++ DirectXInput/Input/InputValidate.cs
@@ -8,21 +8,52 @@ namespace DirectXInput
 {
     partial class WindowMain
     {
-        //Validate controller input data hash
+        //Validate controller input data
         bool InputValidateData(ControllerStatus controllerStatus)
         {
             try
             {
-                if (controllerStatus.Details.Wireless)
+                if (controllerStatus.SupportedCurrent.CodeName == "NintendoSwitchPro")
                 {
-                    if (controllerStatus.SupportedCurrent.CodeName == "SonyPS4DualShock" || controllerStatus.SupportedCurrent.CodeName == "SonyPS5DualSense")
+                    //Check controller report mode
+                    byte check0 = controllerStatus.ControllerDataInput[0];
+                    if (check0 != 0x30) { return false; }
+                }
+                else if (controllerStatus.SupportedCurrent.CodeName == "SonyPS4DualShock")
+                {
+                    if (controllerStatus.Details.Wireless)
+                    {
+                        //Compute MD5
+                        int checksumOffset = controllerStatus.SupportedCurrent.OffsetWireless + (int)controllerStatus.SupportedCurrent.OffsetHeader.Checksum;
+                        byte[] checksumInput = controllerStatus.ControllerDataInput.Take(checksumOffset).ToArray();
+
+                        //Read MD5
+                        byte check0 = controllerStatus.ControllerDataInput[checksumOffset];
+                        byte check1 = controllerStatus.ControllerDataInput[checksumOffset + 1];
+                        byte check2 = controllerStatus.ControllerDataInput[checksumOffset + 2];
+                        byte check3 = controllerStatus.ControllerDataInput[checksumOffset + 3];
+
+                        //Compare 8BitDo static hash
+                        if (check0 == 169 && check1 == 47 && check2 == 73 && check3 == 54) { return true; }
+
+                        //Compare computed MD5 hash
+                        byte[] checksumCompute = ComputeHashCRC32(0x8C2C830C, checksumInput, false);
+                        if (checksumCompute[0] != check0) { return false; }
+                        if (checksumCompute[1] != check1) { return false; }
+                        if (checksumCompute[2] != check2) { return false; }
+                        if (checksumCompute[3] != check3) { return false; }
+                    }
+                }
+                else if (controllerStatus.SupportedCurrent.CodeName == "SonyPS5DualSense")
+                {
+                    if (controllerStatus.Details.Wireless)
                     {
                         //Compute MD5
                         int checksumOffset = controllerStatus.SupportedCurrent.OffsetWireless + (int)controllerStatus.SupportedCurrent.OffsetHeader.Checksum;
                         byte[] checksumInput = controllerStatus.ControllerDataInput.Take(checksumOffset).ToArray();
                         byte[] checksumCompute = ComputeHashCRC32(0x8C2C830C, checksumInput, false);
 
-                        //Compare MD5
+                        //Compare computed MD5 hash
                         byte check0 = controllerStatus.ControllerDataInput[checksumOffset];
                         if (checksumCompute[0] != check0) { return false; }
                         byte check1 = controllerStatus.ControllerDataInput[checksumOffset + 1];
