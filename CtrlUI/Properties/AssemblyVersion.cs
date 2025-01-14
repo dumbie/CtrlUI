36bb152883f37d09f8f7029af55a6b5112f1cf50
Arnold Vink <info@arnoldvink.com>
2025-01-14 16:27:50 +0100

    - Updated project NuGet packages. - Updated assembly version.

--- CtrlUI/Properties/AssemblyVersion.cs
+++ CtrlUI/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.0.1")]
+[assembly: AssemblyFileVersion("2.2.0.1")]
