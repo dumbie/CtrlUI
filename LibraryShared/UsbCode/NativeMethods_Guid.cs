71a5d191d3166f88f7a64c6152ea2b2386711394
Arnold Vink <info@arnoldvink.com>
2025-05-27 10:56:23 +0200

    - DriverInstaller: Updated driver guids.

--- LibraryShared/UsbCode/NativeMethods_Guid.cs
+++ LibraryShared/UsbCode/NativeMethods_Guid.cs
@@ -10,11 +10,11 @@ namespace LibraryUsb
         public static Guid GuidClassHidClass = new Guid("745A17A0-74D3-11D0-B6FE-00A0C90F57DA");
         public static Guid GuidClassWinUsbClass = new Guid("88BAE032-5A81-49f0-BC3D-A4FF138216D6");
         public static Guid GuidClassX360Controller = new Guid("EC87F1E3-C13B-4100-B5F7-8B84D54260CB");
-        public static Guid GuidClassVigemVirtualBus = new Guid("96E42B22-F5E9-42F8-B043-ED0F932F014F");
+        public static Guid GuidClassVigemG1VirtualBus = new Guid("96E42B22-F5E9-42F8-B043-ED0F932F014F");
+        public static Guid GuidClassVigemG2VirtualBus = new Guid("FC68C874-8333-4458-B3F7-79DE124C5587");
         public static Guid GuidClassScpVirtualBus = new Guid("F679F562-3164-42CE-A4DB-E7DDBE723909");
         public static Guid GuidClassScpDS3Driver = new Guid("E2824A09-DBAA-4407-85CA-C8E8FF5F6FFA");
         public static Guid GuidClassHidHideGuardian = new Guid("0C320FF7-BD9B-42B6-BDAF-49FEB9C91649");
-        public static Guid GuidClassFakerInputDevice = new Guid("00FFFE0F-0000-0000-0000-504944564944");
-        public static Guid GuidClassTetherScriptDevice = new Guid("00FFFE0F-0000-0000-0000-504944564944");
+        public static Guid GuidClassFakerInputDevice = new Guid("AB67B0FA-D0F5-4F60-81F4-346E18FD0805");
     }
 }
