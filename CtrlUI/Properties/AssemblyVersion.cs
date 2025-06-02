5f5f514b98e59a810cc189b41366a38bdafaf177
Arnold Vink <info@arnoldvink.com>
2025-06-02 13:44:52 +0200

    - Updated assembly version.

--- CtrlUI/Properties/AssemblyVersion.cs
+++ CtrlUI/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
