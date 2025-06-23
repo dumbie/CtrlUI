4e61b6da0f7266096c0af913e3ee443fcaeb284d
Arnold Vink <info@arnoldvink.com>
2025-06-23 10:58:44 +0200

    - Started adding 4Game launcher support.

---
+++ CtrlUI/Launchers/4GameListApps.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using static ArnoldVinkCode.AVImage;
+using static CtrlUI.AppVariables;
+using static LibraryShared.Classes;
+using static LibraryShared.Enums;
+
+namespace CtrlUI
+{
+    partial class WindowMain
+    {
+        async Task FourGameScanAddLibrary()
+        {
+            try
+            {
+                //Open the Windows registry
+                using (RegistryKey registryKeyCurrentUser = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+                {
+                    using (RegistryKey regKeyUninstall = registryKeyCurrentUser.OpenSubKey("Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"))
+                    {
+                        if (regKeyUninstall != null)
+                        {
+                            //Filter 4Game applications
+                            var regKeyGames = regKeyUninstall.GetSubKeyNames().Where(x => x.StartsWith("4game_")).ToList();
+                            foreach (string appId in regKeyGames)
+                            {
+                                try
+                                {
+                                    using (RegistryKey installDetails = regKeyUninstall.OpenSubKey(appId))
+                                    {
+                                        string displayName = installDetails.GetValue("DisplayName").ToString();
+                                        if (!displayName.Contains("4Game"))
+                                        {
+                                            string applicationId = appId.Replace("4game_global_", string.Empty).Replace("_live", string.Empty);
+                                            string displayIcon = installDetails.GetValue("DisplayIcon").ToString();
+                                            string installLocation = installDetails.GetValue("InstallLocation").ToString();
+                                            string executablePath = Path.Combine(installLocation, "gameManager\\gameManager.exe");
+                                            string executeArguments = "run -l 4game_global -k " + applicationId;
+                                            await FourGameAddApplication(displayName, displayIcon, executablePath, executeArguments);
+                                        }
+                                    }
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed adding 4Game library: " + ex.Message);
+            }
+        }
+
+        async Task FourGameAddApplication(string displayName, string displayIcon, string executablePath, string executeArguments)
+        {
+            try
+            {
+                //Add application to check list
+                vLauncherAppAvailableCheck.Add(executablePath);
+
+                //Check if application is already added
+                DataBindApp launcherExistCheck = List_Launchers.FirstOrDefault(x => x.PathExe.ToLower() == executablePath.ToLower());
+                if (launcherExistCheck != null)
+                {
+                    //Debug.WriteLine("4Game app already in list: " + appName);
+                    return;
+                }
+
+                //Check if application name is ignored
+                string appNameLower = displayName.ToLower();
+                if (vCtrlIgnoreLauncherName.Any(x => x.String1.ToLower() == appNameLower))
+                {
+                    //Debug.WriteLine("Launcher is on the blacklist skipping: " + appName);
+                    await ListBoxRemoveAll(lb_Launchers, List_Launchers, x => x.Name.ToLower() == appNameLower);
+                    return;
+                }
+
+                //Get application image
+                BitmapImage iconBitmapImage = FileToBitmapImage(new string[] { displayName, displayIcon, "4Game" }, vImageSourceFoldersAppsCombined, vImageBackupSource, vImageLoadSize, 0, IntPtr.Zero, 0);
+
+                //Add the application to the list
+                DataBindApp dataBindApp = new DataBindApp()
+                {
+                    Category = AppCategory.Launcher,
+                    Launcher = AppLauncher.FourGame,
+                    Name = displayName,
+                    ImageBitmap = iconBitmapImage,
+                    PathExe = executablePath,
+                    Argument = executeArguments,
+                    StatusLauncherImage = vImagePreload4Game
+                };
+
+                await ListBoxAddItem(lb_Launchers, List_Launchers, dataBindApp, false, false);
+                //Debug.WriteLine("Added 4Game app: " + displayName);
+            }
+            catch
+            {
+                Debug.WriteLine("Failed adding 4Game app: " + displayName);
+            }
+        }
+    }
+}
